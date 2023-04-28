86f72c866c6c52a8750231ce7fb61a56d4b4741a
Nishant <nishant@Nishants-MacBook-Air.local>
2023-04-28 20:14:47 +0530

    [WIP] Add Navbar, Update App & CSS.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Alchemy, Network } from 'alchemy-sdk';
 import { useEffect, useState } from 'react';
+import Navbar from './components/Navbar';
 
 import './App.css';
 
@@ -11,41 +12,78 @@ const settings = {
   network: Network.ETH_MAINNET,
 };
 
-const blockNum =  17137020;
-
-// In this week's lessons we used ethers.js. Here we are using the
 // Alchemy SDK is an umbrella library with several different packages.
-//
-// You can read more about the packages here:
-//   https://docs.alchemy.com/reference/alchemy-sdk-api-surface-overview#api-surface
 const alchemy = new Alchemy(settings);
 
 function App() {
   const [blockNumber, setBlockNumber] = useState();
-  const [blockData, setBlockData] = useState("");
-  // const [currentNetwork, setCurrentNetwork] = useState("");
+  const [blockHashes, setBlockHashes] = useState([]);
+  const [blockGasInfo, setBlockGasInfo] = useState([]);
+  const [topTransactions, setTopTransactions] = useState([]);
+  const [totalTransactions, setTotalTransactions] = useState();
 
-  useEffect(() => {
-    async function displayBlockchainData() {
-      setBlockNumber(await alchemy.core.getBlockNumber());
+  const displayBlockchainData = async () => {
+      //get block data
+      const result = await alchemy.core.getBlock(blockNumber);
 
-      const result = await alchemy.core.getBlock(blockNum);
-      setBlockData(result.hash);
-      // setCurrentNetwork(await alchemy.core.getNetwork().name);
-     // const blockDATA = await alchemy.core.getBlock(blockNum);
-     // console.log(blockData);
+      const gasTarget = -1 * (parseInt((parseInt(result.gasLimit)/2) - parseInt(result.gasUsed)) / (parseInt(result.gasLimit)/2)) * 100;
+      const gasUsedPct = (parseInt(result.gasUsed) / parseInt(result.gasLimit)) * 100;
+
+      // Transactions List 
+      const topTransactions = [];
+
+      for (var i = result.transactions.length-1; i >= result.transactions.length-10; i--) {
+        topTransactions.push(result.transactions[i]);
+      }
+
+      //set react state values
+      setBlockGasInfo([parseInt(result.gasLimit), parseInt(result.gasUsed),parseInt(gasUsedPct),parseInt(gasTarget)]);
+      setBlockHashes([result.hash, result.parentHash]);
+
+      setTotalTransactions(result.transactions.length);
+      setTopTransactions(topTransactions);
+
+      setBlockNumber(parseInt(result.number));
     }
 
-    displayBlockchainData();
+  useEffect(() => {
+    displayBlockchainData()
+  }, [])
 
-
-  });
 
   return (
-
     <div className="App">
-     <strong>Latest Block Number: </strong>{blockNumber}
-    <div><strong>Block Data: </strong>{blockData}</div>
+      <Navbar BLOCKEXPLORER DAPP/>
+    <br />
+    <br />
+    <h3 className="chain__stats">Block Stats</h3>
+      <ul className="chain__stats">
+        <li>TOTAL BLOCKS: {blockNumber}</li>
+        <li>CURRENT BLOCK: {blockNumber}</li>
+        <li>TOTAL TRANSACTIONS: {totalTransactions}</li>
+        <li>GAS LIMIT: {blockGasInfo[0]}</li>
+        <li>GAS USED: {blockGasInfo[1]} ({blockGasInfo[2]}%)</li>
+        <li>GAS TARGET: {blockGasInfo[3]}%</li>
+      </ul>
+    
+      <br />
+      <br />
+      <div><strong>BLOCK DATA:</strong>
+        <br />
+        <br />
+          <div>
+            <strong>BLOCK HASH:</strong> {blockHashes[0]}          
+            <br />
+            <strong>PARENT HASH:</strong> {blockHashes[1]}
+            <br />
+            <br />
+            <br />
+            <strong>TOP 10 TRANSACTIONS: </strong>
+            <div>
+              {topTransactions.map( (txID, index) => <ul>{index+1}. {txID} </ul>)}
+            </div>
+          </div>
+      </div>
     </div>
 
   );
---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'; 
+
+class Navbar extends Component {
+
+  render() {
+    return (
+    <nav>
+      <div className="nav__title">BLOCK EXPLORER</div>
+    </nav>
+    );
+  }
+}
+
+export default Navbar;
